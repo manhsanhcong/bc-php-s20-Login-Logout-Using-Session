f8d5c4ce08dd4175ee3707112591d87556e3749e
manhsanhcong <manhsanhcong.08@gmail.com>
2018-11-23 14:39:08 +0700

    add file

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
+class BlogController extends Controller
+{
+    public function showBlog(Request $request)
+    {
+        if ($request->session()->has('login') && $request->session()->get('login')) {
+            return view('blog');
+        }
+
+        $message = 'Bạn chưa đăng nhập.';
+        $request->session()->flash('not-login', $message);
+
+        return view('login');
+    }
+}
